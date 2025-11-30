e6f2d36da73c3fc9825c9c7798d365ef3c3f790a
CMGSK@main <carlosmgov7k@gmail.com>
2025-11-30 13:31:29 +0100

    criterion

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,3 +1,4 @@
+#[cfg(not(feature = "bench"))]
 macro_rules! run_days {
     ($($day:ident),* $(,)?) => {
         $(
@@ -9,25 +10,70 @@ macro_rules! run_days {
 
                 println!("--- Running {} ---", stringify!($day));
                 println!("Part 1:");
-                //dry run for cache warmup
-                let _ = $day::part1();
                 let t = std::time::Instant::now();
-                let result = $day::part1();
-                let time = t.elapsed();
-                println!("\tResult: {:?}", result);
-                println!("\tTime elapsed: {:.3?}", time);
+                for _ in  0..300 {
+                    let _ = $day::part1();
+                }
+                let avg = t.elapsed() / 300;
+                println!("\tResult: {:?}", $day::part1());
+                println!("\tTime elapsed: {:.3?}", avg);
 
                 println!("Part 2:");
-                //dry run for cache warmup
-                let _ = $day::part2();
                 let t = std::time::Instant::now();
-                let result = $day::part2();
-                let time = t.elapsed();
-                println!("\tResult: {:?}", result);
-                println!("\tTime elapsed: {:.3?}", time);
+                for _ in  0..300 {
+                    let _ = $day::part2();
+                }
+                let avg = t.elapsed() / 300;
+                println!("\tResult: {:?}", $day::part2());
+                println!("\tTime elapsed: {:.3?}", avg);
             )*
         }
     };
 }
 
+#[cfg(not(feature = "bench"))]
 run_days!(day01);
+
+#[cfg(feature = "bench")]
+macro_rules! run_days_benchmark {
+    ($($day:ident),* $(,)?) => {
+        $(
+            pub mod $day;
+        )*
+
+        pub fn run_all() {
+            use criterion::{criterion_group, criterion_main, Criterion};
+            use std::hint::black_box;
+            let mut c = Criterion::default();
+
+            $(
+
+                let mut group = c.benchmark_group(stringify!($day));
+
+                println!("--- Running {} ---", stringify!($day));
+                println!("Part 1:");
+                println!("\tResult: {:?}", $day::part1());
+                group.bench_function("Avg Part 1: ", |x| {
+                    x.iter(|| {
+                        let _ = $day::part1();
+                    })
+                });
+                // println!("\tTime elapsed: {:.3?}", time);
+
+                println!("Part 2:");
+                println!("\tResult: {:?}", $day::part2());
+                group.bench_function("Avg Part 2: ", |x| {
+                    x.iter(|| {
+                        let _ = $day::part2();
+                    })
+                });
+                // println!("\tTime elapsed: {:.3?}", time);
+
+                group.finish();
+            )*
+        }
+    };
+}
+
+#[cfg(feature = "bench")]
+run_days_benchmark!(day01);
